3cd483744912feeaaa91e571eba47c48747c86dd
ZiboneleZ <zibonelez47@gmail.com>
2024-03-05 13:49:11 -0800

    Done with the work-hope it works 200%

--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef SORT_H
+#define SORT_H
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,3 +27,25 @@ void quick_sort(int *array, size_t size);
 int splitor(int *arr, int left, int right, size_t size);
 
 void sorting(int *arr, int left, int right, size_t size);
+
+void shell_sort(int *array, size_t size);
+
+void cocktail_sort_list(listint_t **list);
+
+void counting_sort(int *array, size_t size);
+
+void merge_sort(int *array, size_t size);
+
+void heap_sort(int *array, size_t size);
+
+void heapify(int *array, int idx, int idx2, size_t size);
+
+void radix_sort(int *array, size_t size);
+
+void bitonic_sort(int *array, size_t size);
+
+void quick_sort_hoare(int *array, size_t size);
+
+void sort_deck(deck_node_t **deck);
+
+#endif
